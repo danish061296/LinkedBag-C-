7da8db933f2dc562366171a0c6ce95c77853c995
Danish Siddiqui <dsiddiqui@mail.sfsu.edu>
2019-10-24 23:20:21 -0700

    Initial Commit

---
+++ LinkedBag/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LinkedBag
+//
+//  Created by Danish Siddiqui on 10/24/19.
+//  Copyright © 2019 Danish Siddiqui. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
